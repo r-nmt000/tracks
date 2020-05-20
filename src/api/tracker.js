71377870f3f7f8590b60f382cf8034615bd45ec9
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-21 00:01:20 +0900

    feat: add signup api call boilerplate

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'http://aa8462db.ngrok.io'
+});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, {useContext, useState} from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/authContext";
 
 const SignupScreen = ({ navigation }) =>  {
+  const { state, signup } = useContext(AuthContext);
   const {email, setEmail } = useState('');
   const {password, setPassword } = useState('');
 
@@ -34,7 +36,7 @@ const SignupScreen = ({ navigation }) =>  {
       <Spacer>
         <Button
           title="Sign up"
-          onPress={() => navigation.navigate('SignIn')}
+          onPress={() => signup({email, password})}
         />
       </Spacer>
     </View>
