22e40c173d031f9ef4ccbc463115f5cddb22c936
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-24 20:51:49 +0900

    feat: show map on map component

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from "react-native-safe-area-context";
+import Map from "../components/Map";
 
 const TrackCreateScreen = () =>  {
   return (
-    <View>
-      <Text>Track create screen</Text>
-    </View>
+    <SafeAreaView>
+      <Text h3>Track create screen</Text>
+      <Map/>
+    </SafeAreaView>
   );
 };
 
