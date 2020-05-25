94eeb3729a3dae55726ca6910c923f919121cd14
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-05-25 23:46:54 +0900

    feat: add location permission request

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from "react-native-safe-area-context";
+import { requestPermissionsAsync } from "expo-location";
 import Map from "../components/Map";
 
 const TrackCreateScreen = () =>  {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      await requestPermissionsAsync();
+    } catch (e) {
+      setErr(e);
+    }
+  };
+  useEffect(() => {
+    startWatching();
+  }, []);
   return (
     <SafeAreaView>
       <Text h3>Track create screen</Text>
       <Map/>
+      {err ? <Text>Please enable location service</Text> : null }
     </SafeAreaView>
   );
 };
