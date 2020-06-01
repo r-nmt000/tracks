76b76c87a666eb0e168930ca251671ebc44577ba
Ryusuke Nomoto <r.nmt000@gmail.com>
2020-06-02 01:08:30 +0900

    feat: add save function

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,6 +1,19 @@
 import axios from 'axios';
+import {AsyncStorage} from "react-native";
 
-export default axios.create({
+const instance = axios.create({
   baseURL: 'http://40ba165e3c8d.ngrok.io'
-
 });
+
+instance.interceptors.request.use(
+  async (config) => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Baerer ${token}`;
+    }
+    return config;
+  },
+  (err) => {}
+);
+
+export default instance;
--- src/context/trackContext.js
+++ src/context/trackContext.js
@@ -1,4 +1,5 @@
 import createDataContext from "./createDataContext";
+import trackerApi from '../api/tracker';
 
 const trackReducer = (state, action) => {
   switch(action.type) {
@@ -10,8 +11,8 @@ const trackReducer = (state, action) => {
 const fetchTrack = dispatch => (name) => {
 
 };
-const createTrack = dispatch => (name, locations) => {
-  console.log(name);
+const createTrack = dispatch => async (name, locations) => {
+  await trackerApi.post('/tracks', {name, locations})
 };
 
 export const { Context, Provider } = createDataContext(
